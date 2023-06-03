79f53740dc220c9e747c64e426d969504b487819
SoyM <yayaw0628@foxmail.com>
2023-06-02 19:24:10 -0700

    add el2n metric for filter

--- forgetting_score.sh
+++ forgetting_score.sh
@@ -3,7 +3,3 @@ python data_filter.py --filter_method forggetting --sample_method slice
 python main.py --dataset CIFAR10 --ipc 1 --subset True --filter_method forggetting --sample_method slice
 
 python main.py --dataset CIFAR10 --ipc 10 --subset True --filter_method forggetting --sample_method slice
-
-python main.py --dataset CIFAR10 --ipc 1 --subset False --filter_method forggetting --sample_method slice
-
-python main.py --dataset CIFAR10 --ipc 10 --subset False --filter_method forggetting --sample_method slice
